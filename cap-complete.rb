f9288de0749971c62204b33393b216b4d13040f0
Niko Dittmann <niko@restorm.com>
2008-07-20 16:20:33 +0200

    + initial commit

---
+++ cap-complete.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+COMMAND = 'cap'
+FILEDEPENDENCIES = ['config/deploy.rb']
+
+require File.join(File.dirname(__FILE__), 'rake_and_cap_completion_common')
---
+++ rake-complete.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+COMMAND = 'rake'
+FILEDEPENDENCIES = ['rakefile', 'Rakefile', 'rakefile.rb', 'Rakefile.rb']
+
+require File.join(File.dirname(__FILE__), 'rake_and_cap_completion_common')
